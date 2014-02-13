dbe29b5b803506742d987efb92f08db1f42a1133
mikebjava <stormeagleed@hotmail.com>
2014-02-13 16:16:44 -0500

    Test commit

--- src/net/elitesource/src/DonkieDickShit.java
+++ src/net/elitesource/src/DonkieDickShit.java
@@ -3,8 +3,10 @@ package net.elitesource.src;
 public class DonkieDickShit {
 	private boolean hasPenis;
 	private int amount;
+	private boolean isDonk;
 	public DonkieDickShit(boolean hasPenis, int amount) {
 		this.hasPenis = hasPenis;
 		this.amount = amount;
+		
 	}
 }
