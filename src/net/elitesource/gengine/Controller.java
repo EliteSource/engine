f680781332690dea64c6182b6b0051b3cd9f192d
mikebjava <stormeagleed@hotmail.com>
2014-02-15 22:18:57 -0500

    Added documentation to Controller.class.

--- src/net/elitesource/gengine/Controller.java
+++ src/net/elitesource/gengine/Controller.java
@@ -14,13 +14,32 @@ public class Controller extends Thread implements ActionListener
 	protected boolean listenInput;
 	protected Timer apsTimer;
 
+	/**
+	 * Creates a new controller object with a specified actions per second (how
+	 * many times per second controls are ticked). If aps <= 0 then the aps is
+	 * set to default (60).
+	 * 
+	 * @param game
+	 * @param aps
+	 */
 	public Controller(Game game, int aps)
 	{
 		this.game = game;
 		this.listenInput = true;
-		this.apsTimer = new Timer(1000 / aps, this);
+		if (aps <= 60)
+		{
+			this.apsTimer = new Timer(1000 / aps, this);
+		}
 	}
 
+	/**
+	 * Creates a new controller object with the default actions per second (how
+	 * many times per second controls are ticked). By default, the actions per
+	 * second is 60.
+	 * 
+	 * @param game
+	 * @param aps
+	 */
 	public Controller(Game game)
 	{
 		this.game = game;
@@ -28,17 +47,35 @@ public class Controller extends Thread implements ActionListener
 		this.apsTimer = new Timer(1000 / 60, this);
 	}
 
+	/**
+	 * Returns the game object declared in the constructor.
+	 * 
+	 * @return
+	 */
 	public Game getGame()
 	{
 		return this.game;
 	}
 
+	/**
+	 * Enables this use of this controller. If this isn't called (which it is by
+	 * default by the Game object) the controller will not listen for
+	 * keystrokes.
+	 */
 	public void use()
 	{
 		this.start();
 		this.listenInput = true;
 	}
 
+	/**
+	 * Still a WIP, it does what it says. It reloads the controller (for when
+	 * you set contros in game and need to reload them).
+	 * 
+	 * @see The comments inside this method.
+	 * 
+	 * @throws InterruptedException
+	 */
 	public void reload() throws InterruptedException
 	{
 		this.join();
@@ -47,16 +84,26 @@ public class Controller extends Thread implements ActionListener
 		this.start();
 	}
 
+	/**
+	 * Listens for keystrokes. Called a given number per second (dictated by the
+	 * APS declared in the constructor).
+	 */
 	public void listen()
 	{
 	}
 
+	/**
+	 * Runs this thread. Ignore this. Not too important.
+	 */
 	@Override
 	public void run()
 	{
 		this.apsTimer.start();
 	}
 
+	/**
+	 * Action per second stuff. Again, not important, ignore.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent event)
 	{
